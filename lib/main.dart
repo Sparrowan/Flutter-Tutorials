f038d14dd28cf5e716bdbb993142aa3f9c688b64
Alphius Wambua <alphiuswambua@gmail.com>
2021-05-02 01:09:36 +0300

    Started World Time Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sparrowan_id/pages/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: QuoteList(),
+    home: Home(),
   ));
-}
-
-class QuoteList extends StatefulWidget {
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<String> quotes = [
-    'Be yourself, everyone else is already taken',
-    'I have nothing to declare except my genuis',
-    'The truth is rarely pure and never simple'
-  ];
-
-  Widget quoteTemplate(quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600],
-              ),
-            ),
-            SizedBox(
-              height: 6.0,
-            ),
-            Text(
-              'Sparrowan',
-              style: TextStyle(
-                color: Colors.grey[800],
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Awesome Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
-      ),
-    );
-  }
 }
