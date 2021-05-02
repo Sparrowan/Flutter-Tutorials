87961436fe1c846fa724eb7433821a83277778fc
Alphius Wambua <alphiuswambua@gmail.com>
2021-05-02 23:09:35 +0300

    Added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:sparrowan_id/pages/choose_location.dart';
 import 'package:sparrowan_id/pages/home.dart';
+import 'package:sparrowan_id/pages/loading.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation()
+    },
   ));
 }
