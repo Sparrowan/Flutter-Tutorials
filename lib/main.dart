2332c0dc271e65c9a3da5ad0881c72c7587609e7
Alphius Wambua <alphiuswambua@gmail.com>
2021-05-03 00:16:13 +0300

    Added an API call to the world time API

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:sparrowan_id/pages/loading.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: '/home',
+    initialRoute: '/location',
     routes: {
       '/': (context) => Loading(),
       '/home': (context) => Home(),
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,6 +9,29 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getTime() async {
+    Uri url = Uri.parse("https://worldtimeapi.org/api/timezone/Africa/Nairobi");
+
+    Response response = await get(url);
+    Map data = jsonDecode(response.body);
+    // print(data['datetime']);
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
